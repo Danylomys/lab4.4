4c0e9503efe4fd1600cf83b73394308ba4a8a228
Danylo <danylo.mysyshyn.ri.2023@lpnu.ua>
2023-10-05 00:37:36 +0300

    first commit

---
+++ Project14/Project14.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, R, y;
+	cout << "R = "; cin >> R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -1 - R)
+			y = 1;
+		else
+			if (-1 - R < x && x <= -1)
+				y = -sqrt(R * R - pow(x + 1, 2));
+			else
+				if (-1 < x && x <= 2)
+					y = -R;
+				else
+					y = -R + R / 2 * (x - 2);
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
